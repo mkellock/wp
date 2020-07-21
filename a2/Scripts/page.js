51fef89b1aa3a8f3e45a0a2460cd397319508b5e
Matt Kellock <matt.kellock@iress.com>
2020-07-21 23:00:13 +1000

    Added parallex effect and images

--- a2/Scripts/page.js
+++ a2/Scripts/page.js
@@ -19,7 +19,7 @@ function hideMenu() {
 	hamburger.classList.add('hamburger_hide');
 
 	// Change the menu items attached classes
-	menus.forEach(function(menu) {
+	menus.forEach(function (menu) {
 		menu.classList.remove('menu_show');
 		menu.classList.add('menu_hide');
 	});
@@ -35,7 +35,7 @@ function showMenu() {
 	hamburger.classList.add('hamburger_show');
 
 	// Change the menu items attached classes
-	menus.forEach(function(menu) {
+	menus.forEach(function (menu) {
 		menu.classList.remove('menu_hide');
 		menu.classList.add('menu_show');
 	});
@@ -44,7 +44,9 @@ function showMenu() {
 	menuToggled = true;
 }
 
-function showArticle(hash) {
+function showArticle() {
+	let hash = window.location.hash.replace('#', '');
+
 	// Default the hash to an empty string
 	if (hash == undefined) {
 		hash = '';
@@ -53,12 +55,12 @@ function showArticle(hash) {
 	// If we're going to a page
 	if (hash.length > 0) {
 		// Show the page if the hash matches the id, else hide
-		document.querySelectorAll('article').forEach(function(article) {
+		document.querySelectorAll('article').forEach(function (article) {
 			article.style.display = article.id != hash ? 'none' : 'block';
 		});
 	} else {
 		// Else default to the home page
-		document.querySelectorAll('article').forEach(function(article) {
+		document.querySelectorAll('article').forEach(function (article) {
 			if (article.id == 'home') {
 				article.style.display = 'block';
 			} else {
@@ -66,16 +68,21 @@ function showArticle(hash) {
 			}
 		});
 	}
+
+	// Scroll to the top of the page
+	// Used https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+	document.body.scrollTop = 0; // For Safari
+	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
 // Initialise the page's elements on window load
-window.onload = function() {
+window.onload = function () {
 	// Grab the menu elements
 	hamburger = document.querySelector('.hamburger');
 	menus = document.querySelectorAll('.menu');
 
 	// Attach the scrolling behaviour
-	document.addEventListener('scroll', function() {
+	document.addEventListener('scroll', function () {
 		scroll =
 			(document.documentElement['scrollTop'] || document.body['scrollTop']) /
 			((document.documentElement['scrollHeight'] || document.body['scrollHeight']) -
@@ -98,24 +105,10 @@ window.onload = function() {
 	document.querySelector('.hamburger').onclick = toggleMenu;
 
 	// Set the menu items on click behaviour
-	document.querySelectorAll('.menu, article').forEach(function(menu) {
-		menu.onclick = function() {
-			// Grab the hash
-			hash = new URL(this.firstChild.href).hash;
-
+	document.querySelectorAll('.menu, article').forEach(function (menu) {
+		menu.onclick = function () {
 			// Hide the menu
 			hideMenu();
-
-			// Show the correct article
-			showArticle(hash.replace('#', ''));
-
-			// Set page URL
-			window.location.hash = hash;
-
-			// Scroll to the top of the page
-			// Used https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
-			document.body.scrollTop = 0; // For Safari
-			document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 		};
 	});
 
@@ -123,5 +116,8 @@ window.onload = function() {
 	document.querySelector('#toggleWireframeCSS').onclick = toggleWireframe;
 
 	// Show the homepage/page specified in the URL
-	showArticle(window.location.hash.replace('#', ''));
+	showArticle();
+
+	// Catch the change of page
+	window.addEventListener('hashchange', showArticle);
 };
